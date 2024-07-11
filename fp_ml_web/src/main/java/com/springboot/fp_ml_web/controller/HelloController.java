9de4d3f60e477ae30b6f8a8531efacbbb26780ae
Soyeong_Yun <devysy55@gmail.com~>
2024-07-12 03:21:50 +0900

    feat: 라액트-스프링부트연동 (#DF4-54) by소영

--- fp_ml_web/src/main/java/com/springboot/fp_ml_web/controller/HelloController.java
+++ fp_ml_web/src/main/java/com/springboot/fp_ml_web/controller/HelloController.java
@@ -1,13 +1,14 @@
 package com.springboot.fp_ml_web.controller;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Heollo Controller", description = "설명")
+
 @RestController
 public class HelloController {
 
+    @Operation(summary = "테스트용 api")
     @GetMapping("/api/data")
     public String test() {
         return "Hello, world!";
