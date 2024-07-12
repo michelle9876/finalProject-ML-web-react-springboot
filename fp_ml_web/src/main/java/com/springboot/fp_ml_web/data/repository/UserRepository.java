ef6b3c758b0cf855d35cf28a6fa8e98b10d117b8
kimbg9876 <kimbg9876@gmail.com>
2024-07-12 16:10:24 +0900

    합치기용

--- fp_ml_web/src/main/java/com/springboot/fp_ml_web/data/repository/UserRepository.java
+++ fp_ml_web/src/main/java/com/springboot/fp_ml_web/data/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.springboot.fp_ml_web.data.repository;
 
-public interface UserRepository {
+import com.springboot.fp_ml_web.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByNickName(String nickName);
 }
