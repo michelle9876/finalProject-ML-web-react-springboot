f37cb9eb1508a7a478dc7e4940c796ef1d1b0412
JeaYoon <yuni30@naver.com>
2024-07-15 17:45:04 +0900

    프론트 수정

--- fp_ml_web/src/main/java/com/springboot/fp_ml_web/service/UserService.java
+++ fp_ml_web/src/main/java/com/springboot/fp_ml_web/service/UserService.java
@@ -1,18 +1,18 @@
-package com.springboot.fp_ml_web.service;
-
-import com.springboot.fp_ml_web.data.entity.User;
-import com.springboot.fp_ml_web.data.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-public class UserService {
-    private final UserRepository userRepository;
-    public List<User> getUser(){
-        return this.userRepository.findAll();
-    }
-
-}
+//package com.springboot.fp_ml_web.service;
+//
+//import com.springboot.fp_ml_web.data.entity.User;
+//import com.springboot.fp_ml_web.data.repository.UserRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class UserService {
+//    private final UserRepository userRepository;
+//    public List<User> getUser(){
+//        return this.userRepository.findAll();
+//    }
+//
+//}
