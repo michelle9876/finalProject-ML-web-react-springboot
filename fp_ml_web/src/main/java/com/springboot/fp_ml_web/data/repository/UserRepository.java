529eb33835ab52973a82df409db1d5cf34cff50f
Soyeong124 <devysy55@gmail.com>
2024-07-17 15:37:49 +0900

    충돌 해결

--- fp_ml_web/src/main/java/com/springboot/fp_ml_web/data/repository/UserRepository.java
+++ fp_ml_web/src/main/java/com/springboot/fp_ml_web/data/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.springboot.fp_ml_web.data.repository;
 
 import com.springboot.fp_ml_web.data.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findByNickName(String nickName);
 }
