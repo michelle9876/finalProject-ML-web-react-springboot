59dc337ed2622ffdc3f37091b8e5330a6ef0753a
Eun Seo Choi <eschoi2402@naver.com>
2024-07-29 17:21:21 +0900

    지도api 완성

--- fp_ml_web/src/main/java/com/springboot/fp_ml_web/data/entity/SelectionForMap.java
+++ fp_ml_web/src/main/java/com/springboot/fp_ml_web/data/entity/SelectionForMap.java
@@ -6,16 +6,28 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 
+@Entity
+@Table(name = "selection_for_map")
 @Getter
 @Setter
-@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SelectionForMap {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int selectionForMap_id;
-    private int userId;
-    private String service_industry_category;
-    private String service_industry_name;
+    private Long selectionForMap_id;
 
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "service_industry_name", nullable = false)
+    private String serviceIndustryName;
 }
