211867b19e7947a4b810698a2f9e248c502b3665
Eun Seo Choi <eschoi2402@naver.com>
2024-08-01 18:29:54 +0900

    add pom2

--- fp_ml_web/src/main/front/src/setupProxy.js
+++ fp_ml_web/src/main/front/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080',	// 서버 URL or localhost:설정한포트번호
+      target: 'http://43.203.182.10:8080/',	// 최은서 aws 계정 인스턴스 ip 주소 (프리티어)
       changeOrigin: true,
     })
   );
