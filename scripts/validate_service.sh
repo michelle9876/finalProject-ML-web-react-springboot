b5a1f08618cb35a8a1c656c8a3c8fec24b90a622
Soyeong_Yun <devysy55@gmail.com~>
2024-08-07 03:00:34 +0900

    무중단 배포 실패 후 spring-boot-starter-actuator 추가

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# 새 버전 상태 확인
 NEW_PORT=$(cat /home/ubuntu/current_port.txt)
-for i in {1..10}
+echo "Checking application on port $NEW_PORT"
+for i in {1..30}
 do
-    if curl -s http://localhost:$NEW_PORT/health | grep -q "UP"; then
+    echo "Attempt $i: Checking http://localhost:$NEW_PORT/actuator/health"
+    response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:$NEW_PORT/actuator/health)
+    echo "Response code: $response"
+    if [ $response -eq 200 ]; then
         echo "New version is running successfully on port $NEW_PORT"
         sudo /home/ubuntu/switch_traffic.sh $NEW_PORT
         exit 0
     fi
-    echo "Waiting for application to start... (Attempt $i/10)"
-    sleep 3
+    echo "Waiting for application to start... (Attempt $i/30)"
+    sleep 10
 done
 echo "New version failed to start"
 exit 1
