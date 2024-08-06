35074ea965412a71b07ac9357a7e4c599afc6cc9
Soyeong_Yun <devysy55@gmail.com~>
2024-08-07 02:21:37 +0900

    설치 전 권한주는 코드 추가 BY 소영

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# 디렉토리 및 파일 권한 정리
+sudo chown -R ubuntu:ubuntu /home/ubuntu/app
+sudo chmod -R 775 /home/ubuntu/app
+
 # 애플리케이션 디렉토리 생성
 if [ ! -d /home/ubuntu/app ]; then
     mkdir -p /home/ubuntu/app
